b3425d7dfb1cd45b39491c17c8a991475d373e72
jpmorillom1 <morillojuanpablo2003@gmail.com>
2025-09-29 17:49:43 -0500

    chore(project): 🎉 initial Spring Boot setup with Maven wrapper, gitignore, env template and base structure

---
+++ src/main/java/com/kj/guaufi_api/GuaufiApiApplication.java
@@ -0,0 +1,13 @@
+package com.kj.guaufi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuaufiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuaufiApiApplication.class, args);
+	}
+
+}
